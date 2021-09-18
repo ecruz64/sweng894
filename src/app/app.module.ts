442b798001a1479d8ab4d8d8587faf0dd8081f39
Ernie Cruz <ecruz64@me.com>
2021-09-18 08:30:09 -0400

    changes to app.module.ts for healthy lawn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HLawnComponent } from './h-lawn/h-lawn.component';
-import { WateringComponent } from './watering/watering.component';
-import { MowingComponent } from './mowing/mowing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HLawnComponent,
-    WateringComponent,
-    MowingComponent
+    HLawnComponent
   ],
   imports: [
     BrowserModule
