19704871c1257432e5997fa1545a9a5198e75741
Ernie Cruz <ecruz64@me.com>
2021-09-18 08:18:30 -0400

    modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HLawnComponent } from './h-lawn/h-lawn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HLawnComponent
   ],
   imports: [
     BrowserModule
