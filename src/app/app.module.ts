3511b73b10090dff116ffdd10c2c02bbe824fcea
Ernie Cruz <ecruz64@me.com>
2021-09-18 08:25:24 -0400

    mowing component works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MowingComponent} from "./mowing/mowing.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MowingComponent
   ],
   imports: [
     BrowserModule
