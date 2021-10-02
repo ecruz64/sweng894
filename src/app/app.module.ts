0704ba71939ebe65a79afd2b2a40d254c3188851
Ernie Cruz <ecruz64@me.com>
2021-10-02 07:28:18 -0400

    modified the home page for the merge of the 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {WateringComponent} from "./watering/watering.component";
+import {HLawnComponent} from "./h-lawn/h-lawn.component";
+import {MowingComponent} from "./mowing/mowing.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    WateringComponent
+    WateringComponent,
+    HLawnComponent,
+    MowingComponent
   ],
   imports: [
     BrowserModule
