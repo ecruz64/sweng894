a47d98b11490f37845117e2f4305dc9feb0d7eab
Ernie Cruz <ecruz64@me.com>
2021-10-09 17:44:11 -0400

    added elements to the Mowing App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import  {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import  {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatChipsModule} from "@angular/material/chips";
+
 import { AppComponent } from './app.component';
 import {WateringComponent} from "./watering/watering.component";
 import {HLawnComponent} from "./h-lawn/h-lawn.component";
@@ -11,10 +19,18 @@ import {MowingComponent} from "./mowing/mowing.component";
     AppComponent,
     WateringComponent,
     HLawnComponent,
-    MowingComponent
+    MowingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
