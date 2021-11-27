ea0203bb59e510010658355cf905dfac6aa59767
ernestocruz <ecruz64@me.com>
2021-11-27 14:00:05 -0500

    weather app data convert to Fahrenheit

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -26,9 +26,9 @@ export class WeatherComponent implements OnInit {WeatherData:any;
     this.WeatherData.sunset_time = sunsetTime.toLocaleDateString();
     let currentDate = new Date();
     this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
-    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
-    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+    this.WeatherData.temp_celcius = (((this.WeatherData.main.temp - 273.15)*1.8)+32).toFixed(0);
+    this.WeatherData.temp_min = (((this.WeatherData.main.temp_min - 273.15)*1.8)+32).toFixed(0);
+    this.WeatherData.temp_max = (((this.WeatherData.main.temp_max - 273.15)*1.8)+32).toFixed(0);
+    this.WeatherData.temp_feels_like = (((this.WeatherData.main.feels_like - 273.15)*1.8)+32).toFixed(0);
   }
 }
